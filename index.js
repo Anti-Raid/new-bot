f71b3c67f94564a0d69c5df671c3e22d18be2efe
whiteewolf <arog0660@gmail.com>
2022-11-01 19:27:25 +0200

    test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-require("dotenv").config();
+require('dotenv').config();
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setPresence({
+		activities: { type: ActivityType.Watching, name: 'In Development v6.0.0-alpha' }
+	});
 });
-
 
 client.login(process.env.token);
