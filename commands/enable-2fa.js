8ded53810b5025c2e31a851cd8f1c0e3daf71f8e
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-19 12:47:02 -0600

    Update enable-2fa.js

--- commands/enable-2fa.js
+++ commands/enable-2fa.js
@@ -9,7 +9,7 @@ module.exports = {
 	   const secret = twofactor.generateSecret({ name: "AntiRaid", account: `${interaction.user.username}#${interaction.user.discriminatior}` });
            
            interaction.user.send({
-              content: "Hello, ${interaction.user.username}.\n\nIt seems that you want to enable Two-Factor Authentication, don't worry, the process is extremely simple.\n\nHere is your secret key: /`/`/`${secret.secret}/`/`/`. Just enter this secret key into your Two Factor Authentication app such as Authy, Google Authenticator or Microsoft Authenticator. Once you do this, please send the one-time verification code provided by the authenticator app to confirm all steps were done properly!\n\nIf you do not want to enable 2fa, do not follow any of the instructions provided.`
+              content: `Hello, ${interaction.user.username}.\n\nIt seems that you want to enable Two-Factor Authentication, don't worry, the process is extremely simple.\n\nHere is your secret key: /`/`/`${secret.secret}/`/`/`. Just enter this secret key into your Two Factor Authentication app such as Authy, Google Authenticator or Microsoft Authenticator. Once you do this, please send the one-time verification code provided by the authenticator app to confirm all steps were done properly!\n\nIf you do not want to enable 2fa, do not follow any of the instructions provided.`
            });
         },
 	async autocomplete(client, interaction, database) {},
