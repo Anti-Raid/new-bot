467220e26e78dab0f18e27ec9b04d73c14e0a8e3
whiteewolf <arog0660@gmail.com>
2023-03-14 16:00:55 +0200

    some updates

--- commands/timeout.js
+++ commands/timeout.js
@@ -10,18 +10,32 @@ module.exports = {
       option.setName('user')
         .setDescription('The user to be timed out.')
         .setRequired(true))
-    .addIntegerOption(option =>
-      option.setName('duration')
-        .setDescription('The duration of the timeout in minutes.')
-        .setRequired(true)
-        .addChoices(
-          {name: `60 seconds`, value: 1000*60},
-          {name:`5 minutes`, value: 1000*60*5},
-          {name:`10 minutes`, value: 1000*60*10},
-        {name:`1 hour`, value: 1000*60*60},
-        {name:`1 day`, value: 1000*60*60*24},
-        {name:`1 week`, value: 1000*60*60*24*7}))
-    .addStringOption(option => option.setName("reason").setDescription("The reason for the timeout.").setMaxLength(1024).setRequired(true)),
+    .addStringOption(option => 
+      option
+      .setName("reason")
+      .setDescription("The reason for the timeout.")
+      .setMaxLength(1024)
+      .setRequired(true))
+    .addNumberOption(option => 
+          option.setName('days')
+          .setDescription('Number of days')
+          .setMinValue(0)
+          .setRequired(false))
+    .addNumberOption(option => 
+          option.setName('hours')
+          .setDescription('Number of hours')
+          .setMinValue(0)
+          .setRequired(false))
+    .addNumberOption(option => 
+          option.setName('minutes')
+          .setDescription('Number of minutes')
+          .setMinValue(0)
+          .setRequired(false))
+    .addNumberOption(option => 
+          option.setName('seconds')
+          .setDescription('Number of seconds')
+          .setMinValue(0)
+          .setRequired(false)),
 /**
  * 
  * @param {Client} client 
@@ -31,15 +45,22 @@ module.exports = {
   async execute(client, interaction) {
     const member = interaction.options.getMember('user');
     const user = interaction.options.getUser('user');
-    const duration = interaction.options.getInteger('duration');
+    const days = interaction.options.getNumber('days') || 0;
+    const hours = interaction.options.getNumber('hours') || 0;
+    const minutes = interaction.options.getNumber('minutes') || 0;
+    const seconds = interaction.options.getNumber('seconds') || 0;
+    const totalSeconds = (days * 86400) + (hours * 3600) + (minutes * 60) + seconds;
     const reason = interaction.options.getString("reason");
-    console.log(duration);
+    console.log(totalSeconds);
+    if (totalSeconds === 0) {
+      member.timeout(null, "Timeout Removed | Removed by " + interaction.user.tag);
+  }
     if(member.manageable || member.moderatable) {
-      member.timeout(duration, `${reason} | Issued by: ${interaction.user.tag}`);
+      member.timeout(totalSeconds * 1000, `${reason} | Issued by: ${interaction.user.tag}`);
       interaction.reply({embeds: [new EmbedBuilder()
         .setColor("DarkRed")
         .setAuthor({name: `${interaction.user.tag} | ${interaction.guild.name} (${interaction.guild.nameAcronym})`, iconURL: user.displayAvatarURL({dynamic: true})})
-      .setDescription(`${user.tag} Timeouted for ${require("ms")(duration, {long: true})}`)
+      .setDescription(`${user.tag} Timed out for ${require("ms")(totalSeconds * 1000, {long: true})}`)
     .setTimestamp()]})
     }else{
       return interaction.reply({content:`${user.tag} couldn't be timeouted. Check if i have the permissions to timeout him/her.`})
